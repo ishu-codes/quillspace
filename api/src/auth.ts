3d6e2174a391181bfccd5c78d197338b27746f21
Ishu <kumarishu494@gmail.com>
2026-01-18 12:52:15 +0530

    Minor import bug fixed

--- api/src/auth.ts
+++ api/src/auth.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 dotenv.config();
 
-import { createUserInfoAfterSignUp } from "./auth/plugins";
+import { createUserInfoAfterSignUp } from "./config/authPlugins";
 
 const db = new PrismaClient();
 
